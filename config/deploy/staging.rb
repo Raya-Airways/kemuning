f4a8b51f62638671c0c0de5099d5f763aef119be
maxcobmara <maxcobmara@gmail.com>
2021-08-20 17:19:58 +0800

    update

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,7 +2,7 @@ set :application_name, 'kemuning-raya-staging'
 set :domain, '10.60.20.110'
 set :port, '8586'
 set :deploy_to, '/var/www/kemuning-raya-staging'
-set :repository, 'git@github:Raya-Airways/kemuning.git'
+set :repository, 'git@github-rayaair:Raya-Airways/kemuning.git'
 #set :branch, 'master'  #should use development in future
 set :branch, 'main'
 set :rails_env, 'staging'
