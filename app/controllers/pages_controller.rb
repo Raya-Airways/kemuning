4509cf7406770c1874b37615f788614cd42326bc
Yaacob Noor <yaacob.mn@rayaairways.com>
2021-08-30 12:20:59 +0800

    add documents page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,12 @@
 class PagesController < ApplicationController
 
   def home
-
   end
+
+  def documents
+    @documents = ActiveStorage::Attachment.where(name: "document")
+  end
+
 
   def dashboard
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :positions
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-
+    match '/documents',    to: 'pages#documents',    via: 'get'
     root to: "pages#home"
   end
 end
