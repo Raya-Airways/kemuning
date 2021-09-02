481531938aa93ea98e6a013c427ce42e7618e839
Yaacob Noor <yaacob.mn@rayaairways.com>
2021-09-02 13:27:44 +0800

    test without scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  scope '/kemuning' do
+  #scope '/kemuning' do
     resources :events
     resources :tasks do
       member do
@@ -15,5 +15,5 @@ Rails.application.routes.draw do
 
     match '/documents',    to: 'pages#documents',    via: 'get'
     root to: "pages#home"
-  end
+  #end
 end
