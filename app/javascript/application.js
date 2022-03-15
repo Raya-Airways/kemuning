695d981e0c288bab170cef605b9c2dc93fbb20d4
Yaacob Noor <yaacob.mn@rayaairways.com>
2022-03-15 17:05:13 +0800

    raya login now works, boottrap works, javascript works

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,14 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+import "./src/jquery"
+import "./src/jquery-ui.min"
+import Rails from "@rails/ujs"
+
+
+$(function() {
+  console.log("Aloo Dunya");
+  $('p.replace_by_js').replaceWith("<p>Javascript Enabled!</p>");
+  $( "#dialog" ).dialog();
+})
+Rails.start()
